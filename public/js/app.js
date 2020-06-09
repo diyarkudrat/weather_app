65033dc5498385abf7cab884232c39b1ddee1ee3
Diyar Kudrat <dkudrat@gmail.com>
2020-06-08 21:02:47 -0700

    display data to Front-end

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,28 @@ const tempElement = document.querySelector('.temperature span');
 const locationElement = document.querySelector('.place');
 const dateElement = document.querySelector('.date');
 
-dateElement.textContent = new Date().getDate()
+
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+dateElement.textContent = new Date().getDate() + ", " + monthNames[new Date().getMonth()]
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(searchInput.value);
+    locationElement.textContent = 'Loading...';
+    tempElement.textContent = '';
+    weatherCondition.textContent = '';
+    const weatherApi = fetchWeather + "?city=" + searchInput.value
+    fetch(weatherApi).then(res => {
+        res.json().then(data => {
+            if(data.error) {
+                locationElement.textContent = data.error;
+                tempElement.textContent = '';
+                weatherCondition.textContent = '';
+            } else {
+                locationElement.textContent = data.cityName;
+                fahrenheit = Math.ceil((data.temperature - 273.5) * 9/5 + 32)
+                tempElement.textContent =  fahrenheit + String.fromCharCode(176) + 'F';
+                weatherCondition.textContent = data.description.toUpperCase();
+            }
+        })
+    })
 })
--- utils/data.js
+++ utils/data.js
@@ -3,9 +3,9 @@ const params = require('../config');
 
 const weatherData = (city, callback) => {
     const url = params.weatherMap.URL + encodeURIComponent(city) + '&appid=' + params.weatherMap.SECRET_KEY;
-    // console.log(url)
+    console.log(url)
     request({url, json:true}, (err, {body}) => {
-        // console.log(body)
+        console.log(body)
         if(err) {
             callback("Can't fetch data", undefined)
         } else {
